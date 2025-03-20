10aadb6bcbeec30749f201c54732d522aec43228
rohitsrivastva1 <rohit.managers@gmail.com>
2025-03-21 01:19:41 +0530

    OTP fixed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
-import axiosInstance from "../services/axiosInstance";
+// import axiosInstance from "../services/axiosInstance";
+import axiosInstance from "../api/axiosInstance";
 
 export const AuthContext = createContext();
 
