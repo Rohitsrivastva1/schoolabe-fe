180aaee01ca83dedaca780cb57812fbe1157f071
rohitsrivastva1 <rohit.managers@gmail.com>
2025-03-21 01:16:23 +0530

    OTP fixed

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base API instance
 const api = axios.create({
-  baseURL: "",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "",
   withCredentials: true, // Ensures cookies are sent
 });
 
